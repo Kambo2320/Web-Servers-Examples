1be8fbf68850db607facf14a682da44bbd333576
Kambo2320 <kburke@cougarnet.uh.edu>
2022-11-05 19:38:48 -0500

    Updated web example

--- HelloWeb.py
+++ HelloWeb.py
@@ -11,14 +11,34 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 </body>
 </html>
 '''
+        # ...page template...
 
-    # Handle a GET request.
     def do_GET(self):
+        page = self.create_page()
+        self.send_page(page)
+
+    def create_page(self):
+        values = {
+            'date_time'   : self.date_time_string(),
+            'client_host' : self.client_address[0],
+            'client_port' : self.client_address[1],
+            'command'     : self.command,
+            'path'        : self.path
+        }
+        page = self.Page.format(**values)
+        return page
+        
+    # Handle a GET request.
+    def send_page(self, page):
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.send_header("Content-Length", str(len(self.Page)))
         self.end_headers()
         self.wfile.write(self.Page)
+
+    
+
+
 #----------------------------------------------------------------------
 
 if __name__ == '__main__':
